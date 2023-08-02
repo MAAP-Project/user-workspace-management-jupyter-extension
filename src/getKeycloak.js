352e47253b9efdf7629b422bbdae6f770d00871b
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2023-08-02 14:27:39 -0700

    changed to be updateKeyCloakToken doing the retries

--- src/getKeycloak.js
+++ src/getKeycloak.js
@@ -1,29 +1,22 @@
 
-export var getUserInfo = function(callback, retries=10) {
+export var getUserInfo = function(callback) {
     window.parent._keycloak.loadUserInfo().success(function(profile) {
       callback(profile);
     }).error(async function(err) {
       console.log('Failed to load profile.', err);
-      if (retries > 0) {
-        console.log(`Retrying loading keycloak profile... Attempts left: ${retries}`);
-        console.log("graceal1 before the wait 5 seconds function");
-        await waitThreeSeconds();
-        console.log("graceal1 after the wait 5 seconds function");
-        updateKeycloakToken(300);
-        getUserInfo(callback, retries - 1);
-      } else {
-        console.log('All retries failed to load profile.');
-        callback("error");
-      }
+      console.log("graceal1 about to call update keycloak token");
+      updateKeycloakToken(300);
+      console.log("graceal1 after call to update keycloak token");
+      callback("error");
       //return "error";
     });
   };
 
-  function waitThreeSeconds() {
+  function waitTwoSeconds() {
     return new Promise((resolve) => {
       setTimeout(() => {
         resolve();
-      }, 5000); // 3000 milliseconds = 3 seconds
+      }, 2000); // 2000 milliseconds = 2 seconds
     });
   }
   
@@ -39,6 +32,24 @@ export var getUserInfo = function(callback, retries=10) {
       return window.parent._keycloak.idToken;
   };
   
-  export var updateKeycloakToken = function(seconds) {
-      return window.parent._keycloak.updateToken(seconds);
+  export var updateKeycloakToken = async function(seconds, retries=20) {
+      try {
+        console.log("graceal1 right before the call to updateToken");
+        var refreshed = window.parent._keycloak.updateToken(seconds);
+        console.log("graceal1 and refreshed successful and is");
+        console.log(refreshed);
+        return refreshed;
+      } catch (error) {
+        console.log(`graceal1 and update token failed  after attempts ${retries} with`);
+        console.log(error);
+        
+        if (retries > 0) {
+          await waitTwoSeconds();
+          console.log("graceal1 and after the 2 seconds wait, trying function again");
+          updateKeycloakToken(300, retries-1);
+        } else {
+          console.log("graceal1 max number of retries, done with function");
+        }
+        
+      }
   };
