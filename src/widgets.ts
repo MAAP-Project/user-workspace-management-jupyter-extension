84ab2b99e306864fa663e833388aafc74136ac46
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2025-02-03 13:08:48 -0800

    redid injectSSH function

--- src/widgets.ts
+++ src/widgets.ts
@@ -47,31 +47,56 @@ class UserInfoWidget extends Widget {
 
 export class InjectSSH {
   constructor() {
-
+    console.log("graceal1 in constructor of Inject SSH");
     getUserInfo(function(profile: any) {
+      console.log("graceal1 in getuserinfo callback");
+      var getUrl = new URL(PageConfig.getBaseUrl() + 'jupyter-server-extension/uwm/getAccountInfo');
+      var xhr = new XMLHttpRequest();
+      xhr.onload = function() {
+        if (xhr.status == 200) {
+            let key;
 
-        let key = '';
-        
-        if (profile['public_ssh_keys'] === undefined) {
-            Notification.warning("User's SSH Key undefined. SSH service unavailable.");
-        } else {
-            key = profile['public_ssh_keys'];
+            try {
+              console.log("graceal1 in try with response being");
+              console.log(xhr.response);
+              let response = JSON.parse(xhr.response);
+              console.log(response);
+              console.log(response["profile"]);
+              console.log(response["public_ssh_key"]);
+              key = response["profile"]["public_ssh_key"];
+            } catch (error) {
+              console.log("Bad response from jupyter-server-extension/uwm/getAccountInfo");
+            }
+
+            if (key == undefined || profile == undefined || profile['proxyGrantingTicket'] == undefined) {
+              Notification.warning("User's SSH Key undefined. SSH service unavailable.");
+            } else {
+              let getUrlInjectPublicKey = new URL(PageConfig.getBaseUrl() + "jupyter-server-extension/uwm/injectPublicKey");
+              getUrlInjectPublicKey.searchParams.append("key", key);
+              getUrlInjectPublicKey.searchParams.append("proxyGrantingTicket", profile['proxyGrantingTicket']);
+
+              console.log("graceal1 PGT token from profile is ");
+              console.log(profile['proxyGrantingTicket']);
+
+              let xhrInject = new XMLHttpRequest();
+              xhrInject.onload = function() {
+                  console.log("Checked for/injected user's public key and PGT");
+              };
+              xhrInject.open("GET", getUrlInjectPublicKey.href, true);
+              xhrInject.send(null);
+            }
         }
-
-        let getUrl = new URL(PageConfig.getBaseUrl() + "jupyter-server-extension/uwm/injectPublicKey");
-        getUrl.searchParams.append("key", key);
-
-        if (profile['proxyGrantingTicket'] !== undefined) {
-            getUrl.searchParams.append("proxyGrantingTicket", profile['proxyGrantingTicket']);
+        else {
+            console.log("Error making call to account profile. Status is " + xhr.status + ". Was your MAAP PGT token properly set?");
         }
+      };
 
-        // Make call to back end
-        let xhr = new XMLHttpRequest();
-        xhr.onload = function() {
-            console.log("Checked for/injected user's public key and PGT");
-        };
-        xhr.open("GET", getUrl.href, true);
-        xhr.send(null);
+      xhr.onerror = function() {
+        console.log("Error making call to account profile. Status is " + xhr.status + ". Was your MAAP PGT token properly set?");
+      };
+
+      xhr.open("GET", getUrl.href, true);
+      xhr.send(null);
     });
   }
 }
