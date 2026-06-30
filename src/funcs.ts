38222904581921b2be8d0db7ebfff2f1a0218a61
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2026-06-30 14:17:33 -0700

    updated error message

--- src/funcs.ts
+++ src/funcs.ts
@@ -92,11 +92,11 @@ export async function getPresignedUrl(state: IStateDB, key: any, duration:string
     if (presignedS3Url) {
       resolve(presignedS3Url["url"]);
     } else {
-      Notification.error('Failed to get presigned s3 url. You can see more info about mounted s3 buckets here.', {
+      Notification.error('Failed to get presigned s3 url. Make sure the directory is mounted like my-private-bucket, my-public-bucket, etc.', {
         autoClose: 5000,
         actions: [
           {
-            label: 'here',
+            label: 'More info mounted directories',
             callback: () => window.open('https://docs.maap-project.org/en/latest/system_reference_guide/share_data.html#Share-Data', '_blank')
           }
         ]
