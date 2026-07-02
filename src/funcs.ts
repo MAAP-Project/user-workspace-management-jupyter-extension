bdc6a9d1983c97dd4122da30e83829397ff6b74a
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2026-07-01 18:58:10 -0700

    fixed parsing bug

--- src/funcs.ts
+++ src/funcs.ts
@@ -97,7 +97,8 @@ export async function getPresignedUrl(state: IStateDB, key: any, duration:string
       console.log(presignedS3Url["message"]);
 
       try {
-        const presignedS3UrlParsed = JSON.parse(presignedS3Url); 
+        let presignedS3UrlParsed = presignedS3Url.replace(/^Error:\s*/, "");
+        presignedS3UrlParsed = JSON.parse(presignedS3Url); 
         if (presignedS3UrlParsed["message"]) {
           err_message = presignedS3UrlParsed["message"]
         } 
